d45b40642cefab5dfeaa6e449fcc03d86d1ac5ec
thanhdanh27 <maithanhdanh112@gmail.com.com>
2024-07-07 22:02:07 +0700

    changeHashRouter

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -98,7 +98,7 @@ function Header(props) {
                             <FontAwesomeIcon icon={faBars} />
                         </div>
                         <div className="col-2 part2">
-                            <a href="http://localhost:3000">
+                            <a href="/">
                                 <img className="logo" src={Logo} alt="logo" />
                             </a>
                         </div>
